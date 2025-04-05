ef7590987c0733d2edd95f61d5b30d0f864e34c6
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-04-05 13:24:07 +0500

    feat(home): implement split-view layout for desktop

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_responsive_app/data/places.dart';
+import 'package:flutter_responsive_app/model/place.dart';
 import 'package:flutter_responsive_app/widgets/drawer_widget.dart';
+import 'package:flutter_responsive_app/widgets/place_detail_widget.dart';
 import 'package:flutter_responsive_app/widgets/place_gallery_widget.dart';
 import 'package:flutter_responsive_app/widgets/responsive_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Place selectedPlace = allPlaces[0];
+  void changePlace(Place place) => setState(() {
+    selectedPlace = place;  
+  });
   @override
   Widget build(BuildContext context) {
     final isMobile = ResponsiveWidget.isMobile(context);
@@ -20,24 +32,39 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
-}
 
-Widget buildMobile() {
-  return Column(children: [Expanded(child: PlaceGalleryWidget())]);
-}
+  Widget buildMobile() {
+    return Column(children: [Expanded(child: PlaceGalleryWidget())]);
+  }
 
-Widget buildTablet() {
-  return Row(
-    children: [
-      Expanded(flex: 2, child: DrawerWidget()),
-      Expanded(flex: 5, child: PlaceGalleryWidget()),
-    ],
-  );
-}
+  Widget buildTablet() {
+    return Row(
+      children: [
+        Expanded(flex: 2, child: DrawerWidget()),
+        Expanded(flex: 5, child: PlaceGalleryWidget()),
+      ],
+    );
+  }
 
-Widget buildDesktop() {
-  return Container(
-    color: Colors.orange,
-    child: const Center(child: Text('Desktop View')),
-  );
+  Widget buildDesktop() {
+    return Row(
+      children: [
+        const Expanded(child: DrawerWidget()),
+        Expanded(flex: 3, child: buildBody()),
+      ],
+    );
+  }
+
+  Widget buildBody() {
+    return Container(
+      color: Colors.grey[200],
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Expanded(child: PlaceGalleryWidget()),
+          Expanded(flex: 2, child: PlaceDetailWidget(place: selectedPlace)),
+        ],
+      ),
+    );
+  }
 }
